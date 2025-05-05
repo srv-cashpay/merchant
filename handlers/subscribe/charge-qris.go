48a6864e49db7592c56275f3a548c700d869c2a9
Asep rayana <asep.rayana09@gmail.com>
2025-05-05 15:13:15 +0700

    test midtrans db

--- handlers/subscribe/charge-qris.go
+++ handlers/subscribe/charge-qris.go
@@ -4,12 +4,25 @@ import (
 	"net/http"
 
 	dto "github.com/srv-cashpay/merchant/dto"
+	res "github.com/srv-cashpay/util/s/response"
 
 	"github.com/labstack/echo/v4"
 )
 
 func (h *domainHandler) ChargeQris(c echo.Context) error {
 	var req dto.ChargeRequest
+	userid, ok := c.Get("UserId").(string)
+	if !ok {
+		return res.ErrorBuilder(&res.ErrorConstant.InternalServerError, nil).Send(c)
+	}
+	createdBy, ok := c.Get("CreatedBy").(string)
+	if !ok {
+		return res.ErrorBuilder(&res.ErrorConstant.InternalServerError, nil).Send(c)
+	}
+
+	req.UserID = userid
+	req.CreatedBy = createdBy
+
 	if err := c.Bind(&req); err != nil {
 		return c.JSON(http.StatusBadRequest, echo.Map{
 			"error":   "Invalid request payload",
