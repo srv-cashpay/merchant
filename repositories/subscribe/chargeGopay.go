3dd5e02873f78fed18968c89c3f769e63ef61a3f
Asep rayana <asep.rayana09@gmail.com>
2025-05-18 13:31:12 +0700

    sandbox

--- repositories/subscribe/chargeGopay.go
+++ repositories/subscribe/chargeGopay.go
@@ -10,6 +10,8 @@ import (
 	"time"
 
 	dto "github.com/srv-cashpay/merchant/dto"
+	"github.com/srv-cashpay/merchant/entity"
+	util "github.com/srv-cashpay/util/s"
 )
 
 func (r *subscribeRepository) ChargeGopay(req dto.ChargeRequest) (*dto.GopayResponse, error) {
@@ -42,22 +44,40 @@ func (r *subscribeRepository) ChargeGopay(req dto.ChargeRequest) (*dto.GopayResp
 	client := &http.Client{Timeout: 20 * time.Second}
 	res, err := client.Do(httpReq)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("failed to contact Midtrans")
 	}
 	defer res.Body.Close()
 
 	resBody, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("failed to read Midtrans response")
 	}
 
 	var parsed dto.GopayResponse
 	if err := json.Unmarshal(resBody, &parsed); err != nil {
-		return nil, errors.New("Invalid Midtrans response: " + string(resBody))
+		return nil, errors.New("invalid response from Midtrans")
 	}
 
-	if parsed.StatusCode != "201" {
-		return nil, errors.New("Midtrans error: " + parsed.StatusMessage)
+	// if parsed.StatusCode != "201" {
+	// 	return nil, errors.New("midtrans returned an error: " + parsed.StatusMessage)
+	// }
+
+	// return &parsed,
+
+	tx := entity.Subscribe{
+		ID:              util.GenerateRandomString(),
+		UserID:          req.UserID,
+		CreatedBy:       req.CreatedBy,
+		OrderID:         parsed.OrderID,
+		TransactionID:   parsed.TransactionID,
+		GrossAmount:     req.GrossAmount,
+		PaymentType:     parsed.PaymentType,
+		Status:          parsed.TransactionStatus,
+		TransactionTime: parseTime(parsed.TransactionTime),
+	}
+
+	if err := r.DB.Create(&tx).Error; err != nil {
+		return nil, err
 	}
 
 	return &parsed, nil
