74a712cc738b0fdf4f39bc79d332ded176573def
Asep rayana <asep.rayana09@gmail.com>
2025-06-02 15:24:49 +0700

    role

--- entity/role.go
+++ entity/role.go
@@ -1,8 +1,11 @@
 package entity
 
+import "time"
+
 type Role struct {
-	ID         string `gorm:"primary_key,omitempty" json:"id"`
-	Role       string `gorm:"type:varchar(36)" json:"role"`
-	UserID     string `gorm:"type:varchar(36);index" json:"user_id"`
-	MerchantID string `gorm:"type:varchar(36);index" json:"merchant_id"`
+	ID         string    `gorm:"primary_key,omitempty" json:"id"`
+	Role       string    `gorm:"type:varchar(36)" json:"role"`
+	UserID     string    `gorm:"type:varchar(36);index" json:"user_id"`
+	MerchantID string    `gorm:"type:varchar(36);index" json:"merchant_id"`
+	CreatedAt  time.Time `json:"created_at"`
 }
