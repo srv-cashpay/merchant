a402fc2eaa31a40633cc5e837dcb59b44abd8426
Asep rayana <asep.rayana09@gmail.com>
2025-06-03 10:49:42 +0700

    api payment method

--- entity/payment-method.go
+++ entity/payment-method.go
@@ -7,16 +7,15 @@ import (
 )
 
 type PaymentMethod struct {
-	ID            string           `gorm:"primary_key,omitempty" json:"id"`
-	UserID        string           `gorm:"type:varchar(36);index" json:"user_id"`
-	MerchantID    string           `gorm:"type:varchar(36);index" json:"merchant_id"`
-	PaymentMethod string           `gorm:"payment_method,omitempty" json:"payment_methodpayment_method"`
-	Image         *UploadedPayment `gorm:"foreignKey:PaymentID;references:ID" json:"image"` // perbaikan di sini
-	Status        int              `gorm:"status,omitempty" json:"status"`
-	CreatedBy     string           `gorm:"created_by" json:"created_by"`
-	UpdatedBy     string           `gorm:"updated_by" json:"updated_by"`
-	DeletedBy     string           `gorm:"deleted_by" json:"deleted_by"`
-	CreatedAt     time.Time        `json:"created_at"`
-	UpdatedAt     time.Time        `json:"updated_at"`
-	DeletedAt     gorm.DeletedAt   `gorm:"index" json:"deleted_at"`
+	ID            string         `gorm:"primary_key,omitempty" json:"id"`
+	UserID        string         `gorm:"type:varchar(36);index" json:"user_id"`
+	MerchantID    string         `gorm:"type:varchar(36);index" json:"merchant_id"`
+	PaymentMethod string         `gorm:"payment_method,omitempty" json:"payment_method"`
+	Status        int            `gorm:"status,omitempty" json:"status"`
+	CreatedBy     string         `gorm:"created_by" json:"created_by"`
+	UpdatedBy     string         `gorm:"updated_by" json:"updated_by"`
+	DeletedBy     string         `gorm:"deleted_by" json:"deleted_by"`
+	CreatedAt     time.Time      `json:"created_at"`
+	UpdatedAt     time.Time      `json:"updated_at"`
+	DeletedAt     gorm.DeletedAt `gorm:"index" json:"deleted_at"`
 }
