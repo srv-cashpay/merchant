e219e3f59aff2c0a7cb70cd98993a5d9e7f1c442
Asep rayana <asep.rayana09@gmail.com>
2025-06-07 11:15:00 +0700

    role user api

--- entity/roleuser.go
+++ entity/roleuser.go
@@ -1,8 +1,11 @@
 package entity
 
+import "time"
+
 type RoleUser struct {
-	ID           uint   `gorm:"primaryKey;autoIncrement" json:"id"`
-	RoleID       string `gorm:"type:varchar(36);index,omitempty" json:"role_id"`
-	UserID       string `gorm:"type:varchar(36);index,omitempty" json:"user_id"`
-	PermissionID uint   `gorm:"permission_id;index,omitempty" json:"permission_id"`
+	ID           uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	RoleID       string    `gorm:"type:varchar(36);index,omitempty" json:"role_id"`
+	UserID       string    `gorm:"type:varchar(36);index,omitempty" json:"user_id"`
+	PermissionID uint      `gorm:"permission_id;index,omitempty" json:"permission_id"`
+	CreatedAt    time.Time `json:"created_at"`
 }
