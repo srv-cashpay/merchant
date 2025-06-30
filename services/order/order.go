72aa7b1328039cc10056f8f83404c79d607c77b0
Asep rayana <asep.rayana09@gmail.com>
2025-06-30 09:52:29 +0700

    add order via web

--- services/order/order.go
+++ services/order/order.go
@@ -3,10 +3,10 @@ package order
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 
 	dto "github.com/srv-cashpay/merchant/dto"
 	"github.com/srv-cashpay/merchant/entity"
+	res "github.com/srv-cashpay/util/s/response"
 	"gorm.io/gorm"
 )
 
@@ -17,9 +17,10 @@ func (s *orderService) Order(req dto.OrderRequest) (dto.OrderResponse, error) {
 	err := s.Repo.CheckMerchantDetail(req.MerchantID, &merchantDetail)
 	if err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return dto.OrderResponse{}, fmt.Errorf("merchant detail not found for merchant_id: %s", req.MerchantID)
+			return dto.OrderResponse{}, (&res.ErrorConstant.NotFound)
 		}
 		return dto.OrderResponse{}, err
+
 	}
 	productJSON, err := json.Marshal(req.Product)
 	if err != nil {
