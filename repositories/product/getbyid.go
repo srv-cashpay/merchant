5778834ef346d1461f0a2018ebbc48241827530a
Asep rayana <asep.rayana09@gmail.com>
2025-10-12 20:15:31 +0700

    response product

--- repositories/product/getbyid.go
+++ repositories/product/getbyid.go
@@ -1,6 +1,9 @@
 package product
 
 import (
+	"fmt"
+	"strconv"
+
 	dto "github.com/srv-cashpay/merchant/dto"
 	"github.com/srv-cashpay/product/entity"
 )
@@ -14,9 +17,33 @@ func (b *productRepository) GetById(req dto.GetByIdRequest) (*dto.ProductRespons
 		return nil, err
 	}
 
+	statusMap := map[int]string{
+		1: "active",
+		2: "inactive",
+	}
+
+	createdStatus, err := strconv.Atoi(fmt.Sprintf("%v", tr.Status))
+	if err != nil {
+		return &dto.ProductResponse{}, fmt.Errorf("invalid status value: %v", tr.Status)
+	}
+
+	statusString, ok := statusMap[createdStatus]
+	if !ok {
+		return &dto.ProductResponse{}, fmt.Errorf("invalid status value in database")
+	}
+
 	response := &dto.ProductResponse{
-		ProductName: tr.ProductName,
-		CreatedAt:   dto.Timestamp(tr.CreatedAt),
+		ID:           tr.ID,
+		UserID:       tr.UserID,
+		MerchantID:   tr.MerchantID,
+		Barcode:      tr.Barcode,
+		ProductName:  tr.ProductName,
+		Stock:        tr.Stock,
+		MinimalStock: tr.MinimalStock,
+		Status:       statusString,
+		MerkID:       tr.MerkID,
+		CategoryID:   tr.CategoryID,
+		CreatedAt:    dto.Timestamp(tr.CreatedAt),
 	}
 
 	return response, nil
